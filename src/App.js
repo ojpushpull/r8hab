9d3c23b89f6c8142d8c8d75f2a5193e0a3daba87
Okera Johnson <okerajohnson@gmail.com>
2023-03-13 20:13:50 -0400

    change?

--- src/App.js
+++ src/App.js
@@ -1,49 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react"
 
-const url = 'https://ephemeral-tapioca-4abd88.netlify.app/nytreats.json/'
+const UsingFetch = () => {
+  const [users, setUsers] = useState([])
 
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data)
+      })
+  }
 
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-
-  
-  
-
-
-function App() {
-  const [rehab,setRehab] = React.useState('');  
-
-  fetch(url)
-.then((response) => response.json())
-.then((data) => 
-    this.setState({
-      setRehab: data.results
-}  ) 
-   ) 
- 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-            { setRehab.map((rehab) => (
-               <h1>{setRehab.PROVIDER_CITY}</h1>
-            ))}
-         
-        </a>
-      </header>
+    <div>
+      {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default UsingFetch
